e8748690fbbcf119a1caf468dfc477c60e29c03b
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2026-02-04 21:53:36 +0700

    tipe number dart

---
+++ bin/number.dart
@@ -0,0 +1,13 @@
+void main() {
+  // 1 Integer and Double type variable
+  int age = 25; 
+  double pi = 3.14; 
+  print(age);
+  print(pi);
+
+  // 2 Num type variable
+  num number = 20; 
+  print(number);
+  number = 20.5;
+  print(number);
+}
