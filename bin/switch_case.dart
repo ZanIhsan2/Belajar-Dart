29f850ec7a2d0c53ccce011e0b31a1e9db7ca324
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2026-02-10 00:55:57 +0700

    switch_case dart

---
+++ bin/switch_case.dart
@@ -0,0 +1,17 @@
+void main() {
+  var num = 2;
+
+  switch (num) {
+    case 1 :
+    print("Satu");
+    break;
+    case 2 :
+    print("Dua");
+    break;
+    case 3 :
+    print("Tiga");
+    break;
+    default :
+    print("Bukan Satu, Dua, atau Tiga");
+  }
+}
