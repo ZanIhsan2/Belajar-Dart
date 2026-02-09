b88f9827276b5f10c017a4264edea1242b4239a9
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2026-02-10 01:04:36 +0700

    for_loops dart

---
+++ bin/for_loops.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (var i = 1; i <= 5; i++) {
+    print("Ini adalah angka $i");
+  }
+
+  List<String> fruits = ['apel', 'pisang', 'jeruk'];
+  for (var value in fruits) {
+    print(value);
+  }
+}
